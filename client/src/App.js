f70fbf4133b5ede462e90cd0a5be0daea28a2293
Mayank Kumar <mayankkumar@Mayanks-MacBook-Air.local>
2024-10-22 01:48:41 +0530

    Project Push

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import toast, { Toaster } from 'react-hot-toast';
+
+const App = () => {
+  return (
+    <> 
+      <Toaster/>
+       <main >
+        <Outlet/>
+       </main>
+   </>
+  )
+}
+
+export default App
---
+++ server/models/converstation.model.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      default: "",
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      default: "",
+    },
+    videoUrl: {
+      type: String,
+      default: "",
+    },
+    seen: {
+      type: Boolean,
+      default: false,
+    },
+    msgByUserId: { // Add this field
+      type: mongoose.Schema.ObjectId,
+      required: true,
+      ref: "User", // Reference to the User model
+    },
+  },
+  {
+    timeseries: true,
+  }
+);
+
+const converstationSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    receiver: {
+      type: mongoose.Schema.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    message: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const MessageModel = mongoose.model("Message", messageSchema)
+const ConversationModel = mongoose.model("Conversation", converstationSchema)
+
+module.exports = { MessageModel, ConversationModel }
